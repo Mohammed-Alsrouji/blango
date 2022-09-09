016967866875b238783718f06c2f43b3fe9cf6a1
Mohammed Alsrouji <mhdsq8@gmail.com>
2022-09-09 11:45:45 +0000

    Finish html frameworks

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 # Register your models here.
 admin.site.register(Tag)
@@ -7,6 +7,5 @@ admin.site.register(Tag)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
-admin.site.register(comment)
+admin.site.register(Comment)
