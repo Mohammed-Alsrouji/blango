ad57abeaef9d82d0f78e44a0e7f372185a7271e0
Mohammed Alsrouji <mhdsq8@gmail.com>
2022-12-14 13:14:54 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,3 +1,43 @@
-for(let i=0; i<10; i+=1){
-  console.log("for loop i: ")
+class Greeter{
+  constructor (name){
+    this.name = name
+  }
+
+  getGreeting() {
+    if(this.name === undefined){
+      return "Hello, no name"
+    } 
+    return "Hello, " + this.name
+  }
+
+  showGreeting(greetingMessage){
+    console.log(greetingMessage)
+  }
+
+  greet(){
+    this.showGreeting(this.getGreeting)
+  }
+}
+
+const g = new Greeter("Patch")
+
+
+class DelayedGreeter extends Greeter{
+  delay = 2000
+
+  constructor(name, delay){
+    super(name)
+    if (delay !== undefined){
+      this.delay = delay
+    }
+  }
+
+  greet(){
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )  
+  }
+
 }
