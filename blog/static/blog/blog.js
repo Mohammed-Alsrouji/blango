7c1a37a8e6a21fc9db182a815844c09d395ab99b
Mohammed Alsrouji <mhdsq8@gmail.com>
2022-12-17 14:34:09 +0000

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,43 +1,11 @@
-class Greeter{
-  constructor (name){
-    this.name = name
-  }
-
-  getGreeting() {
-    if(this.name === undefined){
-      return "Hello, no name"
-    } 
-    return "Hello, " + this.name
-  }
-
-  showGreeting(greetingMessage){
-    console.log(greetingMessage)
-  }
-
-  greet(){
-    this.showGreeting(this.getGreeting)
-  }
-}
-
-const g = new Greeter("Patch")
-
-
-class DelayedGreeter extends Greeter{
-  delay = 2000
-
-  constructor(name, delay){
-    super(name)
-    if (delay !== undefined){
-      this.delay = delay
+const lazyAdd = function(a,b){
+  const doAdd = (resolve, reject) => {
+    if(typeof a !== "number" || typeof b !== "number"){
+      reject("a and b must both be number")
+    } else {
+      const sum = a+b
+      resolve(sum)
     }
   }
-
-  greet(){
-    setTimeout(
-      () => {
-        this.showGreeting(this.getGreeting())
-      }, this.delay
-    )  
-  }
-
+  return new Promise(doAdd)
 }
